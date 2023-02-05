86f2917ea247bbf7e0cec1480471eba95f272a43
Chris Barber <cbarber@techquility.local>
2023-02-05 16:08:29 -0500

    first commit

---
+++ server/index.js
@@ -0,0 +1,33 @@
+const { Configuration, OpenAIApi } = require("openai");
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const configuration = new Configuration({
+    organization: "org-sBc15bovT9s1hUPV43cZqROR",
+    apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+
+const app = express()
+app.use(bodyParser.json())
+app.use(cors())
+
+const port = 3080
+
+app.post('/', async (req, res) => {
+    const { message } = req.body;
+    console.log(message, "message")
+    const response = await openai.createCompletion({
+        model: "text-davinci-003",
+        prompt: `${message}`,
+        max_tokens: 100,
+        temperature: 0.5,
+      });      
+    res.json({
+        message: response.data.choices[0].text,
+    })
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+});
